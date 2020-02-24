67b63fbcb8d483f2fbdd24d7080337f9e2664af8
Nicholas de Waal <nick.a.dewaal@gmail.com>
2020-02-24 17:16:48 +0900

    final commit

--- script.js
+++ script.js
@@ -3,17 +3,41 @@ const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
 const countdown = document.getElementById('countdown');
+const loading = document.getElementById('loading');
+
+const year = document.getElementById('year');
 
 const currentYear = new Date().getFullYear();
 
-const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`)
+const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 
-function updateCountdown() {
+// Set background year
+year.innerText = currentYear + 1;
+
+// update countdown time
+function updateCountDown() {
   const currentTime = new Date();
-  constdiff = newYearTime - currentTime;
+  const diff = newYearTime - currentTime;
 
-  console.log(diff);
+  const d = Math.floor(diff/ 1000 / 60 / 60 / 24);
+  const h = Math.floor(diff / 1000 / 60 / 60 ) % 24;
+  const m = Math.floor(diff / 1000 / 60) % 60;
+  const s = Math.floor(diff / 1000 ) % 60;
+
+  // Add vlaues to DOM
+  days.innerHTML = d;
+  hours.innerHTML = h < 10 ? '0'+ h : h;
+  minutes.innerHTML = m < 10 ? '0'+ m : m;
+  seconds.innerHTML = s < 10 ? '0'+ s : s;
 }
 
+// show spinner before
+setTimeout(() => {
+    loading.remove();
+    countdown.style.display = 'flex';
+}, 1000)
+
+// run every second
+setInterval(updateCountDown, 1000);
 
 updateCountDown();
